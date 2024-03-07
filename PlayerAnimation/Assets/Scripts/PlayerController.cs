3f6be74d4b7162c49d69a9c6c3c9a00f7bdce670
Salman Abid <theory252@gmail.com>
2024-03-07 18:19:53 +0500

    Fixing Animation
    Unfortunately I couldn't fix its animation on y-axis. I'll update this repo in future after finding the solution!

---
+++ PlayerAnimation/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables related to player character movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to animation
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1,1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f)) {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+
+        animator.SetFloat("Move X", moveDirection.x);
+        animator.SetFloat("Move Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+
+    void FixedUpdate() 
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
